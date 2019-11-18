2b30fb63b41cc7546a5fd838a089642ea944804c
杉田賢哉 <sugitakenya@sugitakenyanoMacBook-Pro.local>
2019-11-18 17:30:08 +0900

    [Finish]5-9

---
+++ kadai3/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '631124931d18a8393d4e09b28259dce746b0e73c00c34bf1b7d7bd9247cbbce0d3df03e4d7646d89199f398c2519ecdea7b212d6d109f3f6ac994fc4229ada73'
